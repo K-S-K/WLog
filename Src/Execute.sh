9063961d683fb667ed631c41c0f504020454509a
K-S-K <st.kidelevskii@proton.me>
2024-06-18 23:46:57 +0200

    WL-1: Add Empty Project

---
+++ Src/Execute.sh
@@ -0,0 +1,17 @@
+
+# Run multiple processes in a container
+# https://docs.docker.com/config/containers/multi-service_container/
+
+#!/bin/bash
+
+# Start the first process
+./api/WLog.API &
+
+# Start the second process
+./ui/WLog.Site &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
